6dfccf804ca8b11e6375af58912578bebc122608
nouvak <nouvak@gmail.com>
2013-01-22 16:41:38 +0100

    Implemented the delivery cost request form.

--- kcclass-delivery-cost/src/main/java/si/kcclass/deliverycost/controllers/DeliveryCostController.java
+++ kcclass-delivery-cost/src/main/java/si/kcclass/deliverycost/controllers/DeliveryCostController.java
@@ -1,12 +1,18 @@
 package si.kcclass.deliverycost.controllers;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import si.kcclass.deliverycost.domain.Country;
+import si.kcclass.deliverycost.domain.DeliveryCostRequest;
+import si.kcclass.deliverycost.domain.Post;
+import si.kcclass.deliverycost.domain.Test;
 
 @RequestMapping("/deliverycost/**")
 @Controller
@@ -22,12 +28,44 @@ public class DeliveryCostController {
     }*/
     
     @RequestMapping
-    public String deliveryCostForm() {
-    	return "deliverycost/request";
+    public ModelAndView deliveryCostForm(Model uiModel) {
+    	//List<Country> countries =  Country.findAllCountrys();
+    	//List<Post> posts =  Country.findAllPosts();
+    	
+    	List<Country> countries = new ArrayList<Country>();
+    	Country c = new Country();
+    	c.setId(1L);
+    	c.setName("Slovenia");
+    	c.setVersion(0);
+    	countries.add(c);
+    	c = new Country();
+    	c.setId(2L);
+    	c.setName("USA");
+    	c.setVersion(0);
+    	countries.add(c);
+    	
+    	List<Post> posts = new ArrayList<Post>();
+    	Post p = new Post();
+    	p.setId(1L);
+    	p.setName("Hruševje");
+    	p.setZipCode("6225");
+    	posts.add(p);
+    	p = new Post();
+    	p.setId(2L);
+    	p.setName("Postojna");
+    	p.setZipCode("6230");
+    	posts.add(p);
+    	
+    	uiModel.addAttribute("countries", countries);
+    	uiModel.addAttribute("posts", posts);
+    	uiModel.addAttribute("deliveryCostRequest", new DeliveryCostRequest());
+    	
+    	ModelAndView mav = new ModelAndView("deliverycost/request");
+    	return mav;
     }
     
     @RequestMapping(method = RequestMethod.POST)
-    public String deliveryCost() {
+    public String deliveryCost(DeliveryCostRequest deliveryCostRequest) {
     	return "deliverycost/response";
     }
 }
