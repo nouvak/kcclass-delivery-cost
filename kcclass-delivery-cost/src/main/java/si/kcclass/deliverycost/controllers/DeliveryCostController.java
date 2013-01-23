a7d85fa73bef31aa095adccee70ebca286710fd3
marko <marko@fikret>
2013-01-23 11:14:26 +0100

    * Removed the Post class because it is not needed. * Updated the label references. * Implemented proper retrieval of the countries.

--- kcclass-delivery-cost/src/main/java/si/kcclass/deliverycost/controllers/DeliveryCostController.java
+++ kcclass-delivery-cost/src/main/java/si/kcclass/deliverycost/controllers/DeliveryCostController.java
@@ -1,6 +1,5 @@
 package si.kcclass.deliverycost.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -11,52 +10,16 @@ import org.springframework.web.servlet.ModelAndView;
 
 import si.kcclass.deliverycost.domain.Country;
 import si.kcclass.deliverycost.domain.DeliveryCostRequest;
-import si.kcclass.deliverycost.domain.Post;
 
 @RequestMapping("/deliverycost/**")
 @Controller
 public class DeliveryCostController {
-
-    /*@RequestMapping(method = RequestMethod.POST, value = "{id}")
-    public void post(@PathVariable Long id, ModelMap modelMap, HttpServletRequest request, HttpServletResponse response) {
-    }
-
-    @RequestMapping
-    public String index() {
-        return "deliverycost/index";
-    }*/
     
     @RequestMapping
     public ModelAndView deliveryCostForm(Model uiModel) {
-    	//List<Country> countries =  Country.findAllCountrys();
-    	//List<Post> posts =  Country.findAllPosts();
-    	
-    	List<Country> countries = new ArrayList<Country>();
-    	Country c = new Country();
-    	c.setId(1L);
-    	c.setName("Slovenia");
-    	c.setVersion(0);
-    	countries.add(c);
-    	c = new Country();
-    	c.setId(2L);
-    	c.setName("USA");
-    	c.setVersion(0);
-    	countries.add(c);
-    	
-    	List<Post> posts = new ArrayList<Post>();
-    	Post p = new Post();
-    	p.setId(1L);
-    	p.setName("Hruševje");
-    	p.setZipCode("6225");
-    	posts.add(p);
-    	p = new Post();
-    	p.setId(2L);
-    	p.setName("Postojna");
-    	p.setZipCode("6230");
-    	posts.add(p);
-    	
+    	List<Country> countries =  Country.findAllCountrys();
+    	    	
     	uiModel.addAttribute("countries", countries);
-    	uiModel.addAttribute("posts", posts);
     	uiModel.addAttribute("deliveryCostRequest", new DeliveryCostRequest());
     	
     	ModelAndView mav = new ModelAndView("deliverycost/request");
--- kcclass-delivery-cost/src/test/java/si/kcclass/deliverycost/domain/PostDataOnDemand.java
+++
@@ -1,7 +0,0 @@
-package si.kcclass.deliverycost.domain;
-
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-@RooDataOnDemand(entity = Post.class)
-public class PostDataOnDemand {
-}
